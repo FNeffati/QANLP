e2d8c95110352bd720e06b5879a0acc069404212
FNeffati <nebli55mohamed@gmail.com>
2022-11-21 10:39:39 -0500

    Added steps for data preprocessing

--- Project/Preprocessor.py
+++ Project/Preprocessor.py
@@ -1,6 +1,15 @@
 def intro():
     print("Potato Salad")
 
+# Import Data set
+
+# Clean Data Set
+
+# Break Data set into Question Answer pairs
+
+# Try to break Question Answer pairs by topic
+
+# Pick Topic to Explore
 
 if __name__ == '__main__':
     intro()
